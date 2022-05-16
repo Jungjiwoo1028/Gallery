be75ed4a64f9205f6510154fd05d2dfd7c9d6b81
jiwoo99 <madeinkorea1028@gmail.com>
2022-05-16 22:48:18 +0900

    add: client project with react

--- server/server.js
+++ server/server.js
@@ -1,17 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 8000;
-// multer : 파일 업로드를 위해 사용되는 multipart/form-data를
-// 다루기 위한 node.js의 미들웨어
+
 const multer = require("multer");
-// dest: 목적지
 // const upload = multer({ dest: "uploads" });
 const { v4: uuid } = require("uuid");
 const mime = require("mime-types");
 
-// destination: 어디에 저장?
-// filename: 어떤 이름으로 저장?
-// cb: callback, file: 파일정보
 // 만약 파일 저장을 차단하고 오류처리를 하고 싶은 경우가 있다면 cb의 첫번째 인자에 오류 객체를 입력해주면 되요. 아직 따로 검증을 적용하지 않았기 때문에 무조건 null을 입력하게 되고 이 경우는 성공처리가 됩니다.
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, "./uploads"),
@@ -20,7 +15,6 @@ const storage = multer.diskStorage({
 });
 
 // fileFilter.cb에 2번째 인자(boolean)으로 인해 사진을 저장할지 말지 결정한다.
-// fileFilter를 사용함으로써 대용량 사진들을 막을 수 있다.
 const upload = multer({
   storage,
   fileFilter: (req, file, cb) => {
@@ -28,7 +22,7 @@ const upload = multer({
     else cb(new Error("invalid file type"), false);
   },
   limits: {
-    fileSize: 1024 * 1024 * 5, // 5메가바이트까지
+    fileSize: 1024 * 1024 * 5, // 5MB
   },
 });
 
@@ -38,9 +32,8 @@ app.use("/uploads", express.static("uploads"));
 
 // post 요청왔을 때 실행되는 함수
 // 주소 바로 뒤에는 미들웨어의 위치이다.
-// file: 파일정보
 // upload.single: 이름이 imageTest인 이미지 하나를 받겠다는 뜻이다.
-app.post("/upload", upload.single("imageTest"), (req, res) => {
+app.post("/upload", upload.single("img"), (req, res) => {
   console.log(req.file);
   res.json(req.file);
 });
